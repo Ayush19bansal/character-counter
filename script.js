d10938ab0814c8f8480cc6140a54b1e4a400385c
Ayush19bansal <bansalaashu7@gmail.com>
2023-10-14 19:07:42 +0530

    new

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ let texts = document.querySelector("#input");
 let totalCharacter= document.querySelector("#totalCharacters");
 let remainingCharacters= document.querySelector("#remainingCharacters");
 
-texts.addEventListener("keyup", () => {
-    updateCount();
-  });
+texts.addEventListener("keyup",()=>{
+  totalCharacter.innerText = texts.value.length;
+  // console.log(texts.value);
+  remainingCharacters.innerText =
+    texts.getAttribute("maxLength") - texts.value.length;
+ 
+});
   
-  updateCount()
-  
-  function updateCount() {
-    totalCharacter.innerText = texts.value.length;
-    remainingCharacters.innerText =
-      texts.getAttribute("maxLength") - texts.value.length;
-  }
